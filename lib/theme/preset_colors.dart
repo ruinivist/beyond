42da5cb87f4355b2a2e4343be421250ae02f92f8
ruinivist <ruinivist@proton.me>
2026-09-13 16:14:40 +0000

    style: change preset colors

--- lib/theme/preset_colors.dart
+++ lib/theme/preset_colors.dart
@@ -4,17 +4,14 @@
 import 'package:flutter/material.dart';
 
 // ---------- Presets ----------
-
 const presetColors = <({String label, Color color})>[
-  (label: 'Black', color: Color(0xff201c1a)),
-  (label: 'Gray', color: Color(0xff887b73)),
-  (label: 'White', color: Color(0xffffffff)),
-  (label: 'Red', color: Color(0xffdc3f3f)),
-  (label: 'Orange', color: Color(0xffff7626)),
-  (label: 'Yellow', color: Color(0xffffc936)),
-  (label: 'Green', color: Color(0xff4caf50)),
-  (label: 'Teal', color: Color(0xff45c7c4)),
-  (label: 'Blue', color: Color(0xff2f6fde)),
-  (label: 'Purple', color: Color(0xff7928e8)),
-  (label: 'Pink', color: Color(0xffdf267d)),
+  (label: 'Black', color: Color(0xff2b2725)),
+  (label: 'Gray', color: Color.fromARGB(255, 116, 109, 106)),
+  (label: 'Red', color: Color(0xffd85b5b)),
+  (label: 'Orange', color: Color(0xffdf824d)),
+  (label: 'Yellow', color: Color.fromARGB(255, 219, 175, 65)),
+  (label: 'Green', color: Color(0xff68a36c)),
+  (label: 'Blue', color: Color(0xff5c86d6)),
+  (label: 'Purple', color: Color(0xff9674d4)),
+  (label: 'Pink', color: Color(0xffd26598)),
 ];
